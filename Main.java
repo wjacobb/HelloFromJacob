5e08c59f6aa3b6501f010758cef2fb7a34a988ba
gitser16 <samerich16@byui.edu>
2021-10-08 16:31:15 -0500

    Added line with hello message

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is Jacob Williams");
+        System.out.println("Hello Jacob, my name is Sam Richardson");
     }
 }
